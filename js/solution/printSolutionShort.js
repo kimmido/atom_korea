f31c370a2f5e695cdadf5c9c65570ca0809ce517
kimmido <icecream120102@gmail.com>
2025-03-10 17:25:57 +0900

    data 파일 디렉토리 분리

--- js/solution/printSolutionShort.js
+++ js/solution/printSolutionShort.js
@@ -1,17 +1,3 @@
-const iconAlt = {
-    timer: "타이머 아이콘",
-    labors: "노동자 아이콘",
-    chart: "상승차트 아이콘",
-    hourglass: "모래시계 아이콘",
-    machine: "기계부품  아이콘",
-    position: "위치 아이콘",
-    robot_picking: "선별로봇 아이콘",
-    packing: "포장 아이콘",
-    packaging: "2차 포장 아이콘",
-    conveyor_belt: "컨베이어 벨트 아이콘",
-}
-
-
 function printSolutionShort(dataArr) {
     const artBtm = document.querySelector(".artBtm");
     let shortHtml = "";
